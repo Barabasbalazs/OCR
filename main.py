8ab0a4b7559298756b15e5bb69e913a7e9572b48
Barabas Balazs <barabas.balazska@gmail.com>
2023-03-30 09:09:42 +0300

    Visualization of cos centroid working

--- main.py
+++ main.py
@@ -99,7 +99,7 @@ def get_most_common(list):
     return indexes.index(max(indexes))
 
 
-def centroid(training_data, comparison_data):
+def centroid_models(training_data, comparison_data):
     # model_arrays = [[0] * 64] * 10
     rows, cols = (10, 64)
     model_arrays = [[0 for i in range(cols)] for j in range(rows)]
@@ -115,11 +115,12 @@ def centroid(training_data, comparison_data):
     return model_arrays
 
 
-def write_results(training_error, training_numbers, training_euclidean, training_cos, 
+def write_results(filename, training_error, training_numbers, training_euclidean, training_cos, 
                   test_error, test_numbers, test_euclidean, test_cos, k):
-    file = open("output.txt","w")
+    file = open(filename ,"w")
 
-    file.write("K is = {}\n".format(k))
+    if k != -1:
+        file.write("K is = {}\n".format(k))
 
     file.write("Training error on euclidean: {}\n".format(training_error[0]))
     file.write("Training error on models using euclidean\n")
@@ -144,35 +145,113 @@ def write_results(training_error, training_numbers, training_euclidean, training
 
 
 def visualize_number(data_of_image):
-    number_matrix = []
-    it = 0
-    for i in range(8):
-        number_matrix.append([])
-        for j in range(8):
-            number_matrix[i].append(data_of_image[it])
-            it += 1
+    _, ax_arr = plt.subplots(4, 3)
 
-    np_number = np.array(number_matrix)
+    x_index = 0
+    y_index = 0
+    for curr_image in data_of_image:
+        number_matrix = []
+        it = 0
+        for i in range(8):
+            number_matrix.append([])
+            for j in range(8):
+                number_matrix[i].append(curr_image[it])
+                it += 1
+        ax_arr[x_index, y_index].imshow(number_matrix)
+        y_index += 1
+        if y_index > 2:
+            y_index = 0
+            x_index += 1
 
-    plt.imshow(np_number)
+    plt.show()
+
+
+def knn_main_function(training_data, training_numbers, test_data, test_numbers, k):
+    training_error = [0, 0]
+    test_error = [0, 0]
+    training_error[0], training_euclidean = knn(training_data, training_data, "euclidean", k)
+    training_error[1], training_cos = knn(training_data, training_data, "euc", k)
+    test_error[0], test_euclidean = knn(test_data, training_data, "euclidean", k)
+    test_error[1], test_cos = knn(test_data, training_data, "euc", k)
+    write_results("output_knn.txt", training_error, training_numbers, training_euclidean, training_cos,
+                test_error, test_numbers, test_euclidean, test_cos, k)
+
+
+def centroid_compare(data, model, mode):
+    model_counter = [0] * 10 
+    
+    # learning error
+    error_counter = 0
+    for image in data:
+        local_lim = (-1, 1000000) if mode == 'euclidean' else (-1, -2)
+        for i in range(10):
+            if mode == "euclidean":
+                diff = euclidean_diff(model[i][0: 64], image[0: 64])
+                if diff < local_lim[1]:
+                    local_lim = (i ,diff)
+            else:
+                diff = cos_distance(model[i][0: 64], image[0: 64])
+                if diff > local_lim[1]:
+                    local_lim = (i ,diff)
+        if local_lim[0] == image[64]:
+            error_counter += 1
+            model_counter[local_lim[0]] += 1
+    
+    return error_counter / len(data), model_counter
+
+
+def centroid_main_function(training_data, training_numbers, test_data, test_numbers):
+    training_error = [0, 0]
+    test_error = [0, 0]
+    training_models = centroid_models(training_data, training_numbers)
+    training_error[0], training_euclidean = centroid_compare(training_data, training_models, "euclidean")
+    training_error[1], training_cos = centroid_compare(training_data, training_models, "euc")
+    test_error[0], test_euclidean = centroid_compare(test_data, training_models, "euclidean")
+    test_error[1], test_cos = centroid_compare(test_data, training_models, "euc")
+    write_results("output_centroid.txt", training_error, training_numbers, training_euclidean, training_cos,
+                test_error, test_numbers, test_euclidean, test_cos, -1)
+    visualize_number(training_models)
+
+
+def sort_data(test_data):
+    sorted_arr = [0] * 10
+    
+    for i in range(10):
+        sorted_arr[i] = []
+
+    for image in test_data:
+        index = image[64]
+        sorted_arr[index].append(image)
+    
+    tmp_arr = []
+
+    for i in range(10):
+        for curr_im in sorted_arr[i]:
+            tmp_arr.append(curr_im)
+
+    return tmp_arr
+
+
+def centroid_visualization(test_data):
+    rows, cols = (len(test_data), len(test_data))
+    test_image = [[0 for i in range(cols)] for j in range(rows)]
+    sorted_test = sort_data(test_data)
+    
+    for i in range(len(sorted_test)):
+        for j in range(len(sorted_test)):
+            test_image[i][j] = cos_distance(sorted_test[i][0: 64], sorted_test[j][0: 64])
+
+    plt.imshow(test_image)
     plt.show()
 
 
 if __name__ == "__main__":
     training_data = read_input("optdigits.tra")
     test_data = read_input("optdigits.tes")
-    # visualize_number(training_data[3])
     training_numbers = get_results(training_data)
     test_numbers = get_results(test_data)
-    # perfect_models = centroid(training_data, training_numbers)
-    # print(perfect_models)
-    training_error = [0, 0]
-    test_error = [0, 0]
-    k = 5
-    training_error[0], training_euclidean = knn(training_data, training_data, "euclidean", k)
-    training_error[1], training_cos = knn(training_data, training_data, "euc", k)
-    test_error[0], test_euclidean = knn(test_data, training_data, "euclidean", k)
-    test_error[1], test_cos = knn(test_data, training_data, "euc", k)
-    write_results(training_error, training_numbers, training_euclidean, training_cos,
-                test_error, test_numbers, test_euclidean, test_cos, k)
+    centroid_visualization(test_data)
+    # knn_main_function(training_data, training_numbers, test_data, test_numbers, 2)
+    # centroid_main_function(training_data, training_numbers, test_data, test_numbers)
+    
     
